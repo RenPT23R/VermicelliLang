8655da624d7e4eca4681412787eb93aa3a4934e2
RenPT23R <renpt.unity@gmail.com>
2025-01-03 11:19:50 +0000

    Updated basic.py to Vermicelli

--- basic.py
+++ basic.py
@@ -97,50 +97,50 @@ class Position:
 # TOKENS
 #######################################
 
-TT_INT				= 'INT'
-TT_FLOAT    	= 'FLOAT'
-TT_STRING			= 'STRING'
-TT_IDENTIFIER	= 'IDENTIFIER'
+TT_INT				= 'int'
+TT_FLOAT    	= 'float'
+TT_STRING			= 'str'
+TT_IDENTIFIER	= 'id'
 TT_KEYWORD		= 'KEYWORD'
-TT_PLUS     	= 'PLUS'
-TT_MINUS    	= 'MINUS'
-TT_MUL      	= 'MUL'
-TT_DIV      	= 'DIV'
-TT_POW				= 'POW'
-TT_EQ					= 'EQ'
-TT_LPAREN   	= 'LPAREN'
-TT_RPAREN   	= 'RPAREN'
-TT_LSQUARE    = 'LSQUARE'
-TT_RSQUARE    = 'RSQUARE'
-TT_EE					= 'EE'
-TT_NE					= 'NE'
-TT_LT					= 'LT'
-TT_GT					= 'GT'
-TT_LTE				= 'LTE'
-TT_GTE				= 'GTE'
-TT_COMMA			= 'COMMA'
-TT_ARROW			= 'ARROW'
-TT_NEWLINE		= 'NEWLINE'
-TT_EOF				= 'EOF'
+TT_PLUS     	= '+'
+TT_MINUS    	= '-'
+TT_MUL      	= '*'
+TT_DIV      	= '/'
+TT_POW				= '^'
+TT_EQ					= '='
+TT_LPAREN   	= '('
+TT_RPAREN   	= ')'
+TT_LSQUARE    = '['
+TT_RSQUARE    = ']'
+TT_EE					= '=='
+TT_NE					= '!='
+TT_LT					= '<<'
+TT_GT					= '>>'
+TT_LTE				= '<='
+TT_GTE				= '>='
+TT_COMMA			= ','
+TT_ARROW			= '=>'
+TT_NEWLINE		= '\n'
+TT_EOF				= 'VRLL_END_FILE'
 
 KEYWORDS = [
-  'VAR',
-  'AND',
-  'OR',
-  'NOT',
-  'IF',
-  'ELIF',
-  'ELSE',
-  'FOR',
-  'TO',
-  'STEP',
-  'WHILE',
-  'FUN',
-  'THEN',
-  'END',
-  'RETURN',
-  'CONTINUE',
-  'BREAK',
+  'new',
+  '&&',
+  '||',
+  '~~',
+  'if',
+  'elif',
+  'else',
+  'for',
+  'to',
+  'step',
+  'while',
+  'func',
+  '{',
+  '}',
+  'return',
+  'continue',
+  'break',
 ]
 
 class Token:
